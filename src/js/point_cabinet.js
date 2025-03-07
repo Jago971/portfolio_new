2fb95ca06f48b9f97c9d2b3429bfa9ba9ee1f5a7
Matt Mannings <37386150+Jago971@users.noreply.github.com>
2025-03-07 10:25:05 +0000

    debugging - added console logs to permission function to indicate what clasue of it statement stuck in per device

--- src/js/point_cabinet.js
+++ src/js/point_cabinet.js
@@ -44,9 +44,12 @@ export function deviceOrientationPermission() {
       .catch((error) => {
         console.error("Error requesting permission:", error);
       });
+      console.log("ios")
   } else if ("DeviceOrientationEvent" in window) {
     window.addEventListener("deviceorientation", pointCabinet);
+    console.log("android")
   } else {
     window.addEventListener("mousemove", pointCabinet);
+    console.log("desktop")
   }
 }
